bf76e77acca03487d4ba57167ab42bc5b7fb2b08
Andres-AM <andes.acunamarroquin@gmail.com>
2023-06-16 14:21:07 +0200

    update2

---
+++ libraries.R
@@ -0,0 +1,23 @@
+
+## All packages 
+library(tidyverse)
+library(lubridate) 
+library(modelr) 
+# library(kableExtra)
+# library(plotly)
+library(splines)
+library(knitr) 
+library(plotly)
+library(ggrepel)
+library(paletteer)
+library(survival)
+library(survminer)
+# library(flexdashboard)
+# library(reactable)
+
+## Other settings 
+theme_set(theme_bw())
+options(digits=4,dplyr.summarise.inform=F)
+rm(list = ls())
+cat("\014")
+ 
